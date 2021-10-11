6874b6ff0e97f58ecff369f3a36ddf4a21d8b2b0
peter41122 <ruby.kolton@yandex.ru>
2021-10-11 21:28:58 +0900

    add tailwind, reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import reportWebVitals from './reportWebVitals';
+
+// ** Redux, Thunk & Root Reducer Imports
+import thunk from 'redux-thunk'
+import createDebounce from 'redux-debounced'
+import { createStore, applyMiddleware, compose } from 'redux'
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import allReducer from "./reducers";
+
+
+// ** init middleware
+const middleware = [thunk, createDebounce()]
+
+// ** Dev Tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// ** Create store
+const store = createStore(allReducer, {}, composeEnhancers(applyMiddleware(...middleware)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
